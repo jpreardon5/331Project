b29cc7752e8dc5062a4861b75c210cd680c648d2
jpreardon5 <jamespatrickreardon@gmail.com>
2015-04-18 12:15:02 -0400

    Update BerzerkGameWindow.java

--- BerzerkGameWindow.java
+++ BerzerkGameWindow.java
@@ -1,1 +1,36 @@
+import javax.swing.JFrame;
+
+/**
+ * @author JPR
+ *
+ */
+public class BerzerkGameWindow extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * The window contains the Berzerk game Screen.
+	 */
+	public BerzerkGameWindow(){
+		setTitle("Berzerk");
+		Screen screen = new Screen();
+		add(screen);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		pack();
+		setVisible(true);
+		
+	}
+	/**
+	 * Create the Berzerk window.
+	 * @param args Ignored.
+	 */
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		BerzerkGameWindow window = new BerzerkGameWindow();
+	}
+
+}
 
