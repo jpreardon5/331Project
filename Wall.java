9fece7a22411657dbc6ce096b81422da12d24dd0
chappbrown <cwb3587@uncw.edu>
2015-04-21 17:58:49 -0400

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -15,28 +15,18 @@ public class Wall extends StaticScreenObject {
 	/**
 	 * @param location
 	 */
-	public Wall(Point location) {
-		super(location);
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param x
-	 * @param y
-	 */
-	public Wall(int x, int y) {
-		super(x, y);
-		// TODO Auto-generated constructor stub
-	}
 
 	/**
 	 * @param location
-	 * @param length
+	 * @param height
 	 * @param width
 	 */
-	public Wall(Point location, int length, int width) {
-		super(location, length, width);
+	public Wall(Point location, int height, int width) {
+		super(location, height, width);
 		// TODO Auto-generated constructor stub
+	}
+	public Wall(int x, int y,int height, int width){
+		super(x,y,height,width);
 	}
 
 	/* (non-Javadoc)
@@ -45,16 +35,11 @@ public class Wall extends StaticScreenObject {
 	@Override
 	public void draw(Graphics g) {
 		g.setColor(Color.BLUE);
-		g.fillRect(10, 10, 10, 500);   //left wall
-		g.fillRect(760,10,10,500);		//right wall
-		g.fillRect(10, 10,310,10);		//top-left
-		g.fillRect(450, 10, 310, 10);	//top-right
-		g.fillRect(10, 500, 310, 10);	//b-left
-		g.fillRect(450,500,310,10);		//b-right
-		g.fillRect(135,160,10,200);		//left wall of I
-		g.fillRect(635,160,10,200);		//rt wall
-		g.fillRect(135,255,500,10);	//middle wall of I
+		int x = (int)location.getX();
+		int y = (int)location.getY();
+		g.fillRect(x,y,height,width);
 		
 	}
 
 }
+
